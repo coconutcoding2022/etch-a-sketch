e10240af3806dedb484fdfe8d34222c3e0f74862
Tom Brady <coconutcoding2022@gmail.com>
2022-08-27 10:09:21 -0500

    adding existing files

---
+++ main.js
@@ -0,0 +1,66 @@
+const container = document.querySelector('.container');
+const button = document.querySelector('button');
+
+
+function createGrid(height = 16, width = 16) {
+    const insideContainer = document.createElement('div');
+    insideContainer.classList.add('inside-Container');
+    container.appendChild(insideContainer); 
+    for (i = 0; i < height; i++) {
+        const row = document.createElement('div');
+        row.classList.add('grid-row');
+        insideContainer.appendChild(row);
+        
+        
+    
+        for (j = 0; j < width; j++) {
+            const column = document.createElement('div');
+            column.classList.add('grid-column');
+            row.appendChild(column);
+            
+            column.addEventListener('mouseover', function(){
+                column.style.backgroundColor = 'black'
+            })
+        }
+ 
+    } 
+    
+}
+
+function clearGrid() {
+    let insideContainer = document.querySelector('.inside-Container')
+    insideContainer.remove()
+}
+
+
+
+
+button.addEventListener('click', () => {
+    let askHeight = Number(prompt("What height do you want?"));
+    let askWidth = Number(prompt("What width do you want?"));
+
+    while (askHeight > 100 || askWidth > 100) {
+        alert("Pick a height and width below 100")
+        askHeight = Number(prompt("What height do you want?"));
+        askWidth = Number(prompt("What width do you want?"));
+   }
+
+
+    if(askHeight <= 100 ) {
+        createGrid(askHeight, askWidth);
+    }
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
